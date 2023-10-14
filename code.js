a291b229851a5d778c72f3acb0477df716ad6808
shoeb <shoebkhan912928@gmail.com>
2023-10-14 19:26:21 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,5 @@
+// document.getElementById("w1").innerHTML="hello";
+// document.getElementById("w2").innerHTML="shoeb";
+// document.getElementById("w3").innerHTML="khan";
+// let a="name"
+// console.log(a);
